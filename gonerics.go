25de1c4264cd6bbe9d5debc40e4fc12e108c9fe1
Chris Fenner <commits@dlp.rip>
2022-01-22 17:45:11 -0800

    show some more examples of parameterized types and how they can be passed around

--- gonerics.go
+++ gonerics.go
@@ -52,6 +52,45 @@ func (b barTwo) barr() {}
 
 func match[T any] (a, b T) {}
 
+type producer[T any] interface {
+	produce() T
+}
+
+func (b barOne) produce() barTwo {
+	return barTwo{}
+}
+
+func (b fooOne) produce() fooTwo {
+	return fooTwo{}
+}
+
+func produce[B any](a producer[B]) B {
+	return a.produce()
+}
+
+func produce2[B any](a producer[B], b *B) {
+	*b = a.produce()
+}
+
+func produce3[B any, A producer[B]](a A, b *B) {
+	*b = a.produce()
+}
+
+type concreteProducer[T any] struct {
+}
+
+type concreteFooOne struct {
+	concreteProducer[fooTwo]
+}
+
+func (c concreteProducer[T]) produce() T {
+	return *new(T)
+}
+
+func produce4[B any](a concreteProducer[B]) B {
+	return a.produce()
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	ints := newPile[int]()
@@ -67,7 +106,51 @@ func main() {
 		}
 	}
 	match[foo](fooOne{}, fooTwo{})
-	// This won't compile
-	// match[foo](fooOne{}, barTwo{})
 	match[bar](barOne{}, barTwo{})
+	// This won't compile, it produces the error:
+	// cannot use barTwo{} (value of type barTwo) as type foo in
+	// argument to match[foo]:
+	// barTwo does not implement foo (missing fooo method)
+	// match[foo](fooOne{}, barTwo{})
+
+	// This won't compile either:
+	// type fooTwo of fooTwo{} does not match inferred type
+	// fooOne for T
+	// match(fooOne{}, fooTwo{})
+
+	f := fooOne{}
+	var f2 fooTwo
+
+	// This won't compile, because the compiler only performs type
+	// inference for parameters (not return values).
+	// f2 = produce(f)
+	// Instead, we still have to explicitly provide the type:
+	f2 = produce[fooTwo](f)
+
+	// We can fix this by using an output pointer parameter to
+	// trick the caller into giving us the type.
+
+	// This won't compile, because the compiler still can't infer
+	// fooTwo just based on parameter type inference.
+	// produce2(f, &f2)
+	// Instead, we still have to explicitly provide the type:
+	produce2[fooTwo](f, &f2)
+
+	// Now the compiler can infer the correct types [fooOne, fooTwo]
+	// to use with produce3.
+	produce3(f, &f2)
+
+	// We can embed a type with type parameters and use it with
+	// produce3
+	cf := concreteFooOne{}
+	produce3(cf, &f2)
+
+	// We can also (obviously) just call the method on that
+	f2 = cf.produce()
+
+	// We still can't use the produce function with it.
+	// The type inferencer still can't infer B.
+	// f2 = produce(cf)
+	// So we can explicitly provide the type.
+	f2 = produce[fooTwo](cf)
 }
